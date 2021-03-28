2bb311014851beda5db79336a081646eda108d42
ds5678 <jrpritts7@gmail.com>
2021-03-28 12:45:30 -0400

    Add project files.

---
+++ Implementation.cs
@@ -0,0 +1,38 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+
+namespace ExtremeTempDrop
+{
+    public class Implementation : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Debug.Log($"[{Info.Name}] Version {Info.Version} loaded!");
+            Settings.OnLoad();
+        }
+    }
+
+    [HarmonyPatch(typeof(ExperienceModeManager), "GetOutdoorTempDropCelcius")]
+    public class ExperienceModeManager_GetOutdoorTempDropCelcius_Post
+    {
+        private static void Postfix(ref float __result, float numDays)
+        {
+            if (numDays <= Settings.options.declineStartDay) __result = Settings.options.initialDrop;
+            else if (numDays >= Settings.options.declineEndDay) __result = Settings.options.maxDrop;
+            else
+            {
+                int startDay = Settings.options.declineStartDay;
+                int endDay = Settings.options.declineEndDay;
+                float startDrop = Settings.options.initialDrop;
+                float endDrop = Settings.options.maxDrop;
+                __result = (numDays - startDay) / (endDay - startDay) * (endDrop - startDrop) + startDrop;
+            }
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ModSettings;
+
+namespace ExtremeTempDrop
+{
+    class ExtremeTempDropSettings : JsonModSettings
+    {
+        [Name("Preset")]
+        [Choice("Custom","Pilgram","Voyager","Stalker","Interloper")]
+        public int preset = 1;
+
+        [Name("Initial Temperature Drop")]
+        [Description("The global temperature \"drop\" on day 0.")]
+        [Slider(0, 100, 101)]
+        public float initialDrop = 0f;
+
+        [Name("Decline Starting Day")]
+        [Description("The day at which global temperatures start dropping.")]
+        [Slider(0, 200)]
+        public int declineStartDay = 0;
+
+        [Name("Maximum Temperature Drop")]
+        [Description("The maximum decrease in global temperatures.")]
+        [Slider(0, 100, 101)]
+        public float maxDrop = 0f;
+
+        [Name("Decline Ending Day")]
+        [Description("The day at which global temperatures do not drop any farther.")]
+        [Slider(0, 200)]
+        public int declineEndDay = 0;
+        protected override void OnChange(FieldInfo field, object oldValue, object newValue)
+        {
+            base.OnChange(field, oldValue, newValue);
+            if (field.Name == nameof(preset)) UsePreset((int)newValue);
+            else preset = 0;
+
+            if (field.Name == nameof(initialDrop)) maxDrop = Math.Max((float)newValue, maxDrop);
+            else if (field.Name == nameof(maxDrop)) initialDrop = Math.Min((float)newValue, initialDrop);
+            else if (field.Name == nameof(declineStartDay)) declineEndDay = Math.Max((int)newValue, declineEndDay);
+            else if (field.Name == nameof(declineEndDay)) declineStartDay = Math.Min((int)newValue, declineStartDay);
+
+            RefreshGUI();
+        }
+        private void UsePreset(int preset)
+        {
+            switch (preset)
+            {
+                case 1:
+                    initialDrop = 0f;
+                    declineStartDay = 0;
+                    maxDrop = 0f;
+                    declineEndDay = 0;
+                    break;
+                case 2:
+                    initialDrop = 0f;
+                    declineStartDay = 20;
+                    maxDrop = 5f;
+                    declineEndDay = 200;
+                    break;
+                case 3:
+                    initialDrop = 0f;
+                    declineStartDay = 20;
+                    maxDrop = 10f;
+                    declineEndDay = 200;
+                    break;
+                case 4:
+                    initialDrop = 0f;
+                    declineStartDay = 5;
+                    maxDrop = 20f;
+                    declineEndDay = 50;
+                    break;
+            }
+        }
+    }
+    internal static class Settings
+    {
+        internal static ExtremeTempDropSettings options;
+        internal static void OnLoad()
+        {
+            options = new ExtremeTempDropSettings();
+            options.AddToModSettings("Extreme Temp Drop",MenuType.Both);
+        }
+    }
+}
