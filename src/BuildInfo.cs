612f5431bc75bddc5261af1fbf558912fda876e3
Tomasz (ttr) Rudzki <ttr@ttrsite.net>
2024-05-22 10:31:39 +0100

    GetOutdoorTempDropCelcius() is not being called anymore - move logic to Weather.CalculateCurrentTemperature

--- src/BuildInfo.cs
+++ src/BuildInfo.cs
@@ -4,9 +4,9 @@
     {
         public const string Name = "ExtremeTempDrop"; // Name of the Mod.  (MUST BE SET)
         public const string Description = "Alters temperature degradation."; // Description for the Mod.  (Set as null if none)
-        public const string Author = "ds5678, The Illusion"; // Author of the Mod.  (MUST BE SET)
+        public const string Author = "ds5678, The Illusion, ttr"; // Author of the Mod.  (MUST BE SET)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.0.1"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "1.1.0"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
     }
 }
--- src/Implementation.cs
+++ src/Implementation.cs
@@ -1,6 +1,8 @@
 ﻿using Il2Cpp;
 using HarmonyLib;
 using MelonLoader;
+using Il2CppTLD.Gameplay;
+using UnityEngine;
 
 namespace ExtremeTempDrop
 {
@@ -12,21 +14,36 @@ namespace ExtremeTempDrop
             Settings.instance.AddToModSettings("Extreme Temp Drop");
         }
     }
-
-    [HarmonyPatch(typeof(ExperienceModeManager), nameof(ExperienceModeManager.GetOutdoorTempDropCelcius))]
-    public class ExperienceModeManager_GetOutdoorTempDropCelcius
+    [HarmonyPatch(typeof(Weather), nameof(Weather.CalculateCurrentTemperature))]
+    internal class Weather_CalculateCurrentTemperature
     {
-        private static void Postfix(ref float __result, float numDays)
+        public static void Postfix(Weather __instance)
         {
-            if (numDays <= Settings.instance.declineStartDay) __result = Settings.instance.initialDrop;
-            else if (numDays >= Settings.instance.declineEndDay) __result = Settings.instance.maxDrop;
-            else
+            if (GameManager.m_IsPaused)
             {
-                int startDay = Settings.instance.declineStartDay;
-                int endDay = Settings.instance.declineEndDay;
-                float startDrop = Settings.instance.initialDrop;
-                float endDrop = Settings.instance.maxDrop;
-                __result = (numDays - startDay) / (endDay - startDay) * (endDrop - startDrop) + startDrop;
+                return;
+            }
+            if (!InterfaceManager.IsMainMenuEnabled() && !__instance.IsIndoorEnvironment())
+            {
+                float numDays = GameManager.GetTimeOfDayComponent().GetHoursPlayedNotPaused() / 24f;
+                ExperienceMode p = GameManager.m_ExperienceModeManager.GetCurrentExperienceMode();
+                int num = p.m_OutdoorTempDropDayFinal - p.m_OutdoorTempDropDayStart;
+                // revert vanilla chnages
+                //MelonLogger.Msg("Extreme temp vanilla:" + __instance.m_CurrentTemperature);
+                if (numDays >= p.m_OutdoorTempDropDayFinal) __instance.m_CurrentTemperature += p.m_OutdoorTempDropCelsiusMax;
+                else if (numDays < p.m_OutdoorTempDropDayFinal && numDays >= p.m_OutdoorTempDropDayStart && num > 0) __instance.m_CurrentTemperature += Mathf.Lerp(0f, p.m_OutdoorTempDropCelsiusMax, (numDays - p.m_OutdoorTempDropDayStart) / num);
+
+                if (numDays <= Settings.instance.declineStartDay) __instance.m_CurrentTemperature -= Settings.instance.initialDrop;
+                else if (numDays >= Settings.instance.declineEndDay) __instance.m_CurrentTemperature -= Settings.instance.maxDrop;
+                else
+                {
+                    int startDay = Settings.instance.declineStartDay;
+                    int endDay = Settings.instance.declineEndDay;
+                    float startDrop = Settings.instance.initialDrop;
+                    float endDrop = Settings.instance.maxDrop;
+                    __instance.m_CurrentTemperature -= (numDays - startDay) / (endDay - startDay) * (endDrop - startDrop) + startDrop;
+                }
+                //MelonLogger.Msg("Extreme temp:" + __instance.m_CurrentTemperature);
             }
         }
     }
