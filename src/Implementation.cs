6d853c6f4c85dd63f567893f9ad9e845a8ea706c
Tomasz (ttr) Rudzki <ttr@ttrsite.net>
2024-05-22 14:44:21 +0100

    move calculation to separate class/func so it can be used in different mods

--- src/Implementation.cs
+++ src/Implementation.cs
@@ -14,6 +14,35 @@ namespace ExtremeTempDrop
             Settings.instance.AddToModSettings("Extreme Temp Drop");
         }
     }
+    public class Common
+    {
+        // return positive number of how many deg temp should be reduced
+        // this will keep in logic of GetOutdoorTempDropCelcius()
+        public float GetTempDropC()
+        {
+
+            float tempOffset = 0;
+            float numDays = GameManager.GetTimeOfDayComponent().GetHoursPlayedNotPaused() / 24f;
+            ExperienceMode expMode = GameManager.m_ExperienceModeManager.GetCurrentExperienceMode();
+
+            int num = expMode.m_OutdoorTempDropDayFinal - expMode.m_OutdoorTempDropDayStart;
+            // revert vanilla chnages
+            if (numDays >= expMode.m_OutdoorTempDropDayFinal) tempOffset -= expMode.m_OutdoorTempDropCelsiusMax;
+            else if (numDays < expMode.m_OutdoorTempDropDayFinal && numDays >= expMode.m_OutdoorTempDropDayStart && num > 0) tempOffset -= Mathf.Lerp(0f, expMode.m_OutdoorTempDropCelsiusMax, (numDays - expMode.m_OutdoorTempDropDayStart) / num);
+
+            if (numDays <= Settings.instance.declineStartDay) tempOffset += Settings.instance.initialDrop;
+            else if (numDays >= Settings.instance.declineEndDay) tempOffset += Settings.instance.maxDrop;
+            else
+            {
+                int startDay = Settings.instance.declineStartDay;
+                int endDay = Settings.instance.declineEndDay;
+                float startDrop = Settings.instance.initialDrop;
+                float endDrop = Settings.instance.maxDrop;
+                tempOffset += (numDays - startDay) / (endDay - startDay) * (endDrop - startDrop) + startDrop;
+            }
+            return tempOffset;
+        }
+    }
     [HarmonyPatch(typeof(Weather), nameof(Weather.CalculateCurrentTemperature))]
     internal class Weather_CalculateCurrentTemperature
     {
@@ -25,26 +54,12 @@ namespace ExtremeTempDrop
             }
             if (!InterfaceManager.IsMainMenuEnabled() && !__instance.IsIndoorEnvironment())
             {
-                float numDays = GameManager.GetTimeOfDayComponent().GetHoursPlayedNotPaused() / 24f;
-                ExperienceMode p = GameManager.m_ExperienceModeManager.GetCurrentExperienceMode();
-                int num = p.m_OutdoorTempDropDayFinal - p.m_OutdoorTempDropDayStart;
-                // revert vanilla chnages
-                //MelonLogger.Msg("Extreme temp vanilla:" + __instance.m_CurrentTemperature);
-                if (numDays >= p.m_OutdoorTempDropDayFinal) __instance.m_CurrentTemperature += p.m_OutdoorTempDropCelsiusMax;
-                else if (numDays < p.m_OutdoorTempDropDayFinal && numDays >= p.m_OutdoorTempDropDayStart && num > 0) __instance.m_CurrentTemperature += Mathf.Lerp(0f, p.m_OutdoorTempDropCelsiusMax, (numDays - p.m_OutdoorTempDropDayStart) / num);
-
-                if (numDays <= Settings.instance.declineStartDay) __instance.m_CurrentTemperature -= Settings.instance.initialDrop;
-                else if (numDays >= Settings.instance.declineEndDay) __instance.m_CurrentTemperature -= Settings.instance.maxDrop;
-                else
-                {
-                    int startDay = Settings.instance.declineStartDay;
-                    int endDay = Settings.instance.declineEndDay;
-                    float startDrop = Settings.instance.initialDrop;
-                    float endDrop = Settings.instance.maxDrop;
-                    __instance.m_CurrentTemperature -= (numDays - startDay) / (endDay - startDay) * (endDrop - startDrop) + startDrop;
-                }
+                //MelonLogger.Msg("Extreme temp (pre):" + __instance.m_CurrentTemperature);
+                Common p = new Common();
+                __instance.m_CurrentTemperature -= p.GetTempDropC();
                 //MelonLogger.Msg("Extreme temp:" + __instance.m_CurrentTemperature);
             }
         }
     }
+
 }
