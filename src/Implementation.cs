56255baeca732b61e6aa32f746e897db9feaa2b0
Kai Backman <kai@candela.art>
2024-10-21 21:14:36 +0300

    Initial code for progressive temp drop mod
    Key changes:
    1. Internally use negative values for negative temperatures to clarify the arithmetic
    2. Added global disable given this mod can not easily persist state in save files.
    3. Split out the adjustment calculations in separate methods.

--- src/Implementation.cs
+++ src/Implementation.cs
@@ -3,6 +3,7 @@ using HarmonyLib;
 using MelonLoader;
 using Il2CppTLD.Gameplay;
 using UnityEngine;
+using Il2CppTMPro;
 
 namespace ExtremeTempDrop
 {
@@ -16,50 +17,100 @@ namespace ExtremeTempDrop
     }
     public class Common
     {
-        // return positive number of how many deg temp should be reduced
-        // this will keep in logic of GetOutdoorTempDropCelcius()
-        public float GetTempDropC()
+        public static float VanillaAdjust(float numDays)
         {
-
-            float tempOffset = 0;
-            float numDays = GameManager.GetTimeOfDayComponent().GetHoursPlayedNotPaused() / 24f;
             ExperienceMode expMode = GameManager.m_ExperienceModeManager.GetCurrentExperienceMode();
 
             int num = expMode.m_OutdoorTempDropDayFinal - expMode.m_OutdoorTempDropDayStart;
             // revert vanilla chnages
-            if (numDays >= expMode.m_OutdoorTempDropDayFinal) tempOffset -= expMode.m_OutdoorTempDropCelsiusMax;
-            else if (numDays < expMode.m_OutdoorTempDropDayFinal && numDays >= expMode.m_OutdoorTempDropDayStart && num > 0) tempOffset -= Mathf.Lerp(0f, expMode.m_OutdoorTempDropCelsiusMax, (numDays - expMode.m_OutdoorTempDropDayStart) / num);
-
-            if (numDays <= Settings.instance.declineStartDay) tempOffset += Settings.instance.initialDrop;
-            else if (numDays >= Settings.instance.declineEndDay) tempOffset += Settings.instance.maxDrop;
-            else
+            if (numDays >= expMode.m_OutdoorTempDropDayFinal)
             {
-                int startDay = Settings.instance.declineStartDay;
-                int endDay = Settings.instance.declineEndDay;
-                float startDrop = Settings.instance.initialDrop;
-                float endDrop = Settings.instance.maxDrop;
-                tempOffset += (numDays - startDay) / (endDay - startDay) * (endDrop - startDrop) + startDrop;
+                return -expMode.m_OutdoorTempDropCelsiusMax;
+            } else if (numDays < expMode.m_OutdoorTempDropDayFinal && numDays >= expMode.m_OutdoorTempDropDayStart && num > 0)
+            {
+                return -Mathf.Lerp(0f, expMode.m_OutdoorTempDropCelsiusMax, (numDays - expMode.m_OutdoorTempDropDayStart) / num);
+            } else
+            {
+                return 0f;
             }
-            return tempOffset;
+        }
+
+        public static float ScheduledAdjust(float numDays)
+        {
+            if (numDays <= Settings.instance.declineStartDay) return Settings.instance.initialDrop;
+            else if (numDays >= Settings.instance.declineEndDay) return Settings.instance.maxDrop;
+
+            int startDay = Settings.instance.declineStartDay;
+            int endDay = Settings.instance.declineEndDay;
+            float startDrop = Settings.instance.initialDrop;
+            float endDrop = Settings.instance.maxDrop;
+            return (numDays - startDay) / (endDay - startDay) * (endDrop - startDrop) + startDrop;
+        }
+
+        public static float ProgressiveAdjust(float refTemp, float maxAdjust)
+        {
+            float warmPoint = Settings.instance.warmPoint;
+            float coldPoint = Settings.instance.coldPoint;
+            if (refTemp > warmPoint) return 0;
+            else if (refTemp < coldPoint) return maxAdjust;
+
+            // increase drop across range to make weather more extreme but still occasionally benign
+            float pos = (refTemp - warmPoint) / (coldPoint - warmPoint);
+            return Mathf.Lerp(0f, maxAdjust, pos);
+        }
+
+        // return positive number of how many deg temp should be reduced
+        // this will keep in logic of GetOutdoorTempDropCelcius()
+        public static float GetTempDropC()
+        {
+            // TODO: what should this do?
+            return 0.0f;
         }
     }
+
+
     [HarmonyPatch(typeof(Weather), nameof(Weather.CalculateCurrentTemperature))]
     internal class Weather_CalculateCurrentTemperature
     {
         public static void Postfix(Weather __instance)
         {
+            if (!Settings.instance.enableExtremeTempDrop) return;
             if (GameManager.m_IsPaused)
             {
                 return;
             }
             if (!InterfaceManager.IsMainMenuEnabled() && !__instance.IsIndoorEnvironment())
             {
-                //MelonLogger.Msg("Extreme temp (pre):" + __instance.m_CurrentTemperature);
-                Common p = new Common();
-                __instance.m_CurrentTemperature -= p.GetTempDropC();
-                //MelonLogger.Msg("Extreme temp:" + __instance.m_CurrentTemperature);
+                float t0 = __instance.m_CurrentTemperature;
+                float numDays = GameManager.GetTimeOfDayComponent().GetHoursPlayedNotPaused() / 24f;
+                float vanilla = Common.VanillaAdjust(numDays);
+                float scheduled = Common.ScheduledAdjust(vanilla);
+                if (Settings.instance.enableProgressive)
+                {
+                    float ambient = __instance.GetCurrentTemperatureWithoutHeatSources();
+                    float progressive = Common.ProgressiveAdjust(ambient, scheduled);
+                    float t1 = t0 - vanilla + progressive;
+#if DEBUG
+                    MelonLogger.Msg("Extreme temp: t1 " + t1 +
+                        " = t0 " + t0 +
+                        " - vanilla " + vanilla +
+                        " + progressive " + progressive +
+                        " (ambient " + ambient + " scheduled " + scheduled + ")");
+#endif
+                    __instance.m_CurrentTemperature = t1;
+                }
+                else
+                {
+                    float t1 = t0 - vanilla + scheduled;
+#if DEBUG
+                    MelonLogger.Msg("Extreme temp: t1 " + t1 +
+                        " = t0 " + t0 +
+                        " - vanilla " + vanilla +
+                        " + scheduled " + scheduled);
+#endif
+                    __instance.m_CurrentTemperature = t1;
+                }
             }
         }
     }
-
 }
--- src/Settings.cs
+++ src/Settings.cs
@@ -8,40 +8,74 @@ namespace ExtremeTempDrop
     {
         internal static Settings instance = new Settings();
 
+        [Name("Enable")]
+        [Description("If enabled will replace the base game temperature drop with a custom profile. Might stack with other mods like Relentless Night, disable if in doubt.")]
+        public bool enableExtremeTempDrop = false;
+
         [Name("Preset")]
-        [Description("The default preset values from the base game. If you're using Relentless Night and don't want this to stack with its temperature degradation, you need to use the Pilgram preset.")]
-        [Choice("Custom", "Pilgram", "Voyager", "Stalker", "Interloper")]
+        [Description("The default preset values from the base game. Extreme is a progressive preset where blizzards are almost immediately Interloper deadly but mild weather remains possible.")]
+        [Choice("Custom", "Pilgrim", "Voyager", "Stalker", "Interloper", "Extreme")]
         public int preset = 1;
 
         [Name("Initial Temperature Drop")]
         [Description("The global temperature \"drop\" on day 0.")]
-        [Slider(0, 100, 101)]
+        [Slider(0, -100, 101)]
         public float initialDrop = 0f;
+
+        [Name("Maximum Temperature Drop")]
+        [Description("The maximum decrease in global temperatures.")]
+        [Slider(0, -100, 101)]
+        public float maxDrop = 0f;
 
         [Name("Decline Starting Day")]
         [Description("The day at which global temperatures start dropping.")]
         [Slider(0, 200)]
         public int declineStartDay = 0;
 
-        [Name("Maximum Temperature Drop")]
-        [Description("The maximum decrease in global temperatures.")]
-        [Slider(0, 100, 101)]
-        public float maxDrop = 0f;
-
         [Name("Decline Ending Day")]
         [Description("The day at which global temperatures do not drop any farther.")]
         [Slider(0, 200)]
         public int declineEndDay = 0;
+
+        [Section("Progressive")]
+
+        [Name("Progressive")]
+        [Description("Enabling progressive will apply the temperature drop along the given range between the Warm and Cold points. Close to the Warm Point temperatures remain mostly unchanged, close to the Cold Point most of the drop will be felt.")]
+        public bool enableProgressive = false;
+
+        [Name("Warm Point")]
+        [Description("Temperatures warmer than this point will not be adjusted")]
+        [Slider(0, -100, 101)]
+        public float warmPoint = -10f;
+
+        [Name("Cold Point")]
+        [Description("Temperatures colder than this will receive full adjustment")]
+        [Slider(0, -100, 101)]
+        public float coldPoint = -40f;
+
         protected override void OnChange(FieldInfo field, object? oldValue, object? newValue)
         {
             base.OnChange(field, oldValue, newValue);
             if (field.Name == nameof(preset)) UsePreset((int)newValue);
             else preset = 0;
 
-            if (field.Name == nameof(initialDrop)) maxDrop = Math.Max((float)newValue, maxDrop);
-            else if (field.Name == nameof(maxDrop)) initialDrop = Math.Min((float)newValue, initialDrop);
+            if (field.Name == nameof(initialDrop)) maxDrop = Math.Min((float)newValue, maxDrop);
+            else if (field.Name == nameof(maxDrop)) initialDrop = Math.Max((float)newValue, initialDrop);
             else if (field.Name == nameof(declineStartDay)) declineEndDay = Math.Max((int)newValue, declineEndDay);
             else if (field.Name == nameof(declineEndDay)) declineStartDay = Math.Min((int)newValue, declineStartDay);
+            else if (field.Name == nameof(warmPoint)) coldPoint = Math.Min((float)newValue, coldPoint);
+            else if (field.Name == nameof(coldPoint)) warmPoint = Math.Max((float)newValue, warmPoint);
+            else if (field.Name == nameof(enableExtremeTempDrop))
+            {
+//                bool enabled = (bool)newValue;
+// FIXME toggle visibility
+            }
+            else if (field.Name == nameof(enableProgressive))
+            {
+ //               bool enabled = (bool)newValue;
+ //               SetFieldVisible(warmPoint, enabled);
+ //               SetFieldVisible(coldPoint, enabled);
+            }
 
             RefreshGUI();
         }
@@ -51,27 +85,40 @@ namespace ExtremeTempDrop
             {
                 case 1:
                     initialDrop = 0f;
-                    declineStartDay = 0;
                     maxDrop = 0f;
+                    declineStartDay = 0;
                     declineEndDay = 0;
+                    enableProgressive = false;
                     break;
                 case 2:
                     initialDrop = 0f;
+                    maxDrop = -5f;
                     declineStartDay = 20;
-                    maxDrop = 5f;
                     declineEndDay = 200;
+                    enableProgressive = false;
                     break;
                 case 3:
                     initialDrop = 0f;
+                    maxDrop = -10f;
                     declineStartDay = 20;
-                    maxDrop = 10f;
                     declineEndDay = 200;
+                    enableProgressive = false;
                     break;
                 case 4:
                     initialDrop = 0f;
+                    maxDrop = -20f;
                     declineStartDay = 5;
-                    maxDrop = 20f;
                     declineEndDay = 50;
+                    enableProgressive = false;
+                    break;
+                case 5:
+                    initialDrop = -5f;
+                    maxDrop = -20f;
+                    declineStartDay = 1;
+                    declineEndDay = 5;
+                    enableProgressive = true;
+                    warmPoint = -10.0f;
+                    coldPoint = -40.0f;
                     break;
             }
         }
